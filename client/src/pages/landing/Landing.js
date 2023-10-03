01482237607fdfeeb8f3183093c4adbbf709115f
Nomah <3rdtimecoder@gmail.com>
2023-10-03 21:22:03 +0200

    register now works

--- client/src/pages/landing/Landing.js
+++ client/src/pages/landing/Landing.js
@@ -12,22 +12,9 @@ function LandingPage() {
         password: '',
         confirmPassword: '',
     });
-
-    const register = (e) => {
-        e.preventDefault();
-        const form = document.getElementById('form');
-        const elements = form.elements;
-
-        const newFormData = {
-            username: elements.username.value,
-            email: elements.email.value,
-            password: elements.password.value,
-            confirmPassword: elements['confirm-password'].value,
-        };
-
-        setFormData(newFormData);
-
-        const options = {
+    
+    const getOptions = () => {
+        return {
             method: 'POST', 
             headers: {
               'Content-Type': 'application/json',
@@ -36,54 +23,73 @@ function LandingPage() {
             },
             body: JSON.stringify(formData), 
           };
+    }
 
+    const register = (e) => {
+        e.preventDefault();
+
+        const options = getOptions();
         const formMessage = document.getElementById('form-message');
         formMessage.innerHTML = `<p>Processing...</em>...</p>`;
 
-        fetch(`${config.base_api_url}/register`, options)
-          .then(response => response.json())
-          .then(data => {
-              console.log(data);
-              if (data.result === "OK") navigateTo('/home');
-              else formMessage.innerHTML = data.data.message;
-          })
-          .catch(e => formMessage.innerHTML = "An Error Occured");
+        console.log(`email: ${formData.email}\npassword: ${formData.password}\nconfirm: ${formData.confirmPassword}`);
 
+        if (formData.password !== formData.confirmPassword) {
+            formMessage.innerHTML = `<p><em>Passwords do not match</em>...</p>`;
+        }
+        else {
+            fetch(`${config.base_api_url}/register`, options)
+              .then(response => response.json())
+              .then(data => {
+                  console.log(data);
+                  if (data.result === "OK") navigateTo('/home');
+                  else formMessage.innerHTML = data.data.message;
+              })
+              .catch(e => formMessage.innerHTML = "An Error Occured");
+        }
     }
 
     return (
         <>
-            <div class="container">
-                <div class="text">
-                    <h1 class="heading">&lt; DevZone /&gt;</h1>
+            <div className="container">
+                <div className="text">
+                    <h1 className="heading">&lt; DevZone /&gt;</h1>
                 </div>
 
-                <div class="form-container">
+                <div className="form-container">
                     <form id="form">
-                        <div class="form-control">
-                            <label for="username" class="form-label">username</label>
-                            <input name="username" type="text" id="username" class="form-input" required/>
+                        <div className="form-control">
+                            <label for="username" className="form-label">username</label>
+                            <input name="username" type="text" id="username" className="form-input" required
+                                value={formData.username} 
+                                onChange={(e) => setFormData({...formData, username: e.target.value})} />
                         </div>
             
-                        <div class="form-control">
-                            <label for="email" class="form-label">email</label>
-                            <input name="email" type="email" id="email" class="form-input" required/>
-                        </div>   
+                        <div className="form-control">
+                            <label for="email" className="form-label">email</label>
+                            <input name="email" type="email" id="email" className="form-input" required
+                                value={formData.email} 
+                                onChange={(e) => setFormData({...formData, email: e.target.value})}/>
+                            </div>   
             
-                        <div class="form-control">
-                            <label for="password" class="form-label">password</label>
-                            <input name="password" type="password" id="password" minlength="8" class="form-input" required/>
-                        </div>
+                        <div className="form-control">
+                            <label for="password" className="form-label">password</label>
+                            <input name="password" type="password" id="password" minlength="8" className="form-input" required
+                                value={formData.password} 
+                                onChange={(e) => setFormData({...formData, password: e.target.value})}/>
+                            </div>
             
-                        <div class="form-control">
-                            <label for="confirm-password" class="form-label">confirm password</label>
-                            <input name="confirm-password" type="password" id="confirm-password" minlength="8" class="form-input" required/>
-                        </div>
+                        <div className="form-control">
+                            <label for="confirm-password" className="form-label">confirm password</label>
+                            <input name="confirm-password" type="password" id="confirm-password" minlength="8" className="form-input" required
+                                value={formData.confirmPassword} 
+                                onChange={(e) => setFormData({...formData, confirmPassword: e.target.value})}/>
+                            </div>
 
-                        <div id="form-message" class="form-message"></div>
+                        <div id="form-message" className="form-message"></div>
             
-                        <div class="form-btns">
-                            <input name="register" type="submit" value="Register" class="btn" onClick={register}/>
+                        <div className="form-btns">
+                            <input name="register" type="submit" value="Register" className="btn" onClick={register}/>
                             <input name="login" type="submit" value="Login" class="btn"/>
                         </div>
                     </form>
